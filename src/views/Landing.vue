8896c1c2f6bd9caf6cd9d8a0534e9b522022e93f
yimsb01 <yimsb02@naver.com>
2020-05-18 18:53:07 +0900

    Add vuetify

---
+++ src/views/Landing.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    Landing Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Landing"
+};
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,14 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "test message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
   });
+});
+
+test("1 is 1", () => {
+  expect(1).toBe(1);
 });
