3bdc1866019866dde51f2519bc3bcf624e587d34
yimsb01 <yimsb02@naver.com>
2020-05-19 21:37:28 +0900

    Implement footer layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <v-app id="day-planner">
-    <router-view />
+  <v-app id="day-planner" style="height: 100%;">
+    <router-view id="app-body" />
+    <FooterComponent />
   </v-app>
 </template>
 
 <script>
+import FooterComponent from "@/components/footer/FooterComponent";
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    FooterComponent
+  }
 };
 </script>
 
 <style scoped>
 #day-planner {
-  background-image: url("assets/bg.jpg");
+  background-image: url("assets/img/bg.jpg");
   background-position: center;
   background-size: cover;
 }
---
+++ src/components/footer/FooterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-footer class="pa-0" fixed>
+    <v-container class="py-0">
+      <v-row>
+        <v-col class="d-flex justify-center px-0">
+          <img src="@/assets/img/account-outline.svg" width="48px" />
+        </v-col>
+        <v-col class="d-flex justify-center px-0">
+          <img src="@/assets/img/calendar-blank-outline.svg" width="48px" />
+        </v-col>
+        <v-col class="d-flex justify-center px-0">
+          <img src="@/assets/img/dots-horizontal.svg" width="48px" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent"
+};
+</script>
