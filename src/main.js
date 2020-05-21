701b3016212247f409079079b5257a5d11bade6c
yimsb01 <yimsb02@naver.com>
2020-05-21 19:14:39 +0900

    Apply vue2-touch-events

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import vue2touchevents from "./plugins/vue2-touch";
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  vue2touchevents,
   render: h => h(App)
 }).$mount("#app");
