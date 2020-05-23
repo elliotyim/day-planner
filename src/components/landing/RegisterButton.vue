4aa6fc0444e641925ae88b1755c4308caf4c176f
yimsb01 <yimsb02@naver.com>
2020-05-23 21:45:16 +0900

    Implement signin view layout

--- src/components/landing/RegisterButton.vue
+++ src/components/landing/RegisterButton.vue
@@ -1,9 +1,9 @@
 <template>
   <v-row>
     <v-col align="center" class="px-12">
-      <v-btn class="d-flex white--text" color="#739F90" min-width="100%">
-        Create an Account
-      </v-btn>
+      <v-btn class="d-flex white--text" color="#739F90" block
+        >Create an Account</v-btn
+      >
     </v-col>
   </v-row>
 </template>
--- src/components/landing/SigninButton.vue
+++ src/components/landing/SigninButton.vue
@@ -1,13 +1,7 @@
 <template>
   <v-row>
     <v-col align="center" class="px-12">
-      <v-btn
-        color="white"
-        class="d-flex"
-        min-width="100%"
-        outlined
-        link
-        :to="'/signin/'"
+      <v-btn color="white" class="d-flex" block outlined :to="'/signin/'"
         >Signin</v-btn
       >
     </v-col>
