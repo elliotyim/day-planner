de74069b5ae613e2977aeb0f0ab6848853d1410a
yimsb01 <yimsb02@naver.com>
2020-05-23 18:24:56 +0900

    Rename signin button

---
+++ src/components/landing/SigninButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col align="center" class="px-12">
+      <v-btn
+        color="white"
+        class="d-flex"
+        min-width="100%"
+        outlined
+        link
+        :to="'/signin/'"
+        >Signin</v-btn
+      >
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SigninButton"
+};
+</script>
