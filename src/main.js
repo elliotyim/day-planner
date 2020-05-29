a521b4b3ac837a832c87cc9c6c8071cd8abd9ea3
yimsb01 <yimsb02@naver.com>
2020-05-29 10:31:26 +0900

    Import vue-100vh and remove unnecessary library

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import vue2touchevents from "./plugins/vue2-touch";
 
 Vue.config.productionTip = false;
 
@@ -11,6 +10,5 @@ new Vue({
   router,
   store,
   vuetify,
-  vue2touchevents,
   render: h => h(App)
 }).$mount("#app");
