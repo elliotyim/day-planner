7fc2dbca2775f8ef552fe394257f73dc850df060
yimsb01 <yimsb02@naver.com>
2020-06-02 07:57:13 +0900

    Add icons

--- src/common/constants/icon.js
+++ src/common/constants/icon.js
@@ -2,10 +2,20 @@ const MYPAGE = "fas fa-user";
 const CALENDAR = "fas fa-calendar";
 const TODO = "fas fa-clipboard-list";
 const SETTINGS = "fas fa-cog";
+const CIRCLE = "far fa-circle";
+const CROSS = "fas fa-times";
+const CHECK = "fas fa-check";
+const SQUARE = "far fa-square";
+const STAR = "far fa-star";
 
 export default {
   MYPAGE: MYPAGE,
   CALENDAR: CALENDAR,
   TODO: TODO,
-  SETTINGS: SETTINGS
+  SETTINGS: SETTINGS,
+  CIRCLE: CIRCLE,
+  CROSS: CROSS,
+  SQUARE: SQUARE,
+  CHECK: CHECK,
+  STAR: STAR
 };
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,6 +6,6 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "fas"
+    iconfont: ["fas", "far"]
   }
 });
