44cb09f3cf9788306d0cc9173f33bfbdc04ee8c9
yimsb01 <yimsb02@naver.com>
2020-06-10 18:09:27 +0900

    Modify the date fixtures

--- src/common/fixtures/dateManager.js
+++ src/common/fixtures/dateManager.js
@@ -1,22 +1,24 @@
 import dates from "./dates";
 
 let data = dates.map(a => ({ ...a }));
+let pk = data[data.length - 1].id + 1;
 
 const get = () => {
   return (() => {
     return new Promise(resolve => {
       setTimeout(() => {
-        resolve(data);
+        resolve(data.map(d => ({ ...d })).sort((a, b) => a.color - b.color));
       }, 100);
     });
   })();
 };
 
-const post = async date => {
-  const post = date => {
+const post = async payload => {
+  const post = payload => {
+    let date = { id: pk++, dates: new Date(), ...payload };
     data.push(date);
   };
-  const timer = await setTimeout(post(date), 100);
+  const timer = await setTimeout(post(payload), 100);
   clearTimeout(timer);
 };
 
@@ -24,8 +26,7 @@ const deleteDate = async payload => {
   const deleteDate = payload => {
     data = data.filter(
       d =>
-        d.dates.getDate() !== payload.today.getDate() ||
-        d.popover.label !== payload.label
+        d.dates.getDate() !== new Date().getDate() || d.label !== payload.label
     );
   };
   const timer = await setTimeout(deleteDate(payload), 100);
--- src/common/fixtures/dates.js
+++ src/common/fixtures/dates.js
@@ -1,167 +1,105 @@
 let dates = [
   {
     id: 1,
-    dot: {
-      color: "red"
-    },
-    popover: {
-      label: "Coding"
-    },
+    color: 0,
+    label: "Coding",
     dates: new Date(2020, 5, 17)
   },
   {
     id: 2,
-    dot: {
-      color: "red"
-    },
-    popover: {
-      label: "Coding"
-    },
+    color: 0,
+    label: "Coding",
     dates: new Date(2020, 5, 18)
   },
   {
     id: 3,
-    dot: {
-      color: "green"
-    },
-    popover: {
-      label: "Workout"
-    },
+    color: 1,
+    label: "Workout",
     dates: new Date(2020, 5, 17)
   },
   {
     id: 4,
-    dot: {
-      color: "green"
-    },
-    popover: {
-      label: "Workout"
-    },
+    color: 1,
+    label: "Workout",
     dates: new Date(2020, 5, 18)
   },
   {
     id: 5,
-    dot: {
-      color: "green"
-    },
-    popover: {
-      label: "Workout"
-    },
+    color: 1,
+    label: "Workout",
     dates: new Date(2020, 5, 23)
   },
   {
     id: 6,
-    dot: {
-      color: "blue"
-    },
-    popover: {
-      label: "Reading Books"
-    },
+    color: 2,
+    label: "Reading Books",
     dates: new Date(2020, 5, 17)
   },
   {
     id: 7,
-    dot: {
-      color: "blue"
-    },
-    popover: {
-      label: "Reading Books"
-    },
+    color: 2,
+    label: "Reading Books",
     dates: new Date(2020, 5, 18)
   },
   {
     id: 8,
-    dot: {
-      color: "blue"
-    },
-    popover: {
-      label: "Reading Books"
-    },
+    color: 2,
+    label: "Reading Books",
     dates: new Date(2020, 5, 26)
   },
   {
     id: 9,
-    dot: {
-      color: "purple"
-    },
-    popover: {
-      label: "Math"
-    },
+    color: 3,
+    label: "Math",
     dates: new Date(2020, 5, 17)
   },
   {
     id: 10,
-    dot: {
-      color: "purple"
-    },
-    popover: {
-      label: "Math"
-    },
+    color: 3,
+    label: "Math",
     dates: new Date(2020, 5, 18)
   },
   {
     id: 11,
-    dot: {
-      color: "purple"
-    },
-    popover: {
-      label: "Math"
-    },
+    color: 3,
+    label: "Math",
     dates: new Date(2020, 5, 26)
   },
   {
     id: 12,
-    dot: {
-      color: "yellow",
-      class: "my-dot-class"
-    },
-    popover: {
-      label: "Homework"
-    },
+    color: 4,
+    label: "Homework",
     dates: new Date(2020, 5, 17)
   },
   {
     id: 13,
-    dot: {
-      color: "yellow",
-      class: "my-dot-class"
-    },
-    popover: {
-      label: "Homework"
-    },
+    color: 4,
+    label: "Homework",
     dates: new Date(2020, 5, 18)
   },
   {
     id: 14,
-    dot: {
-      color: "yellow",
-      class: "my-dot-class"
-    },
-    popover: {
-      label: "Homework"
-    },
+    color: 4,
+    label: "Homework",
     dates: new Date(2020, 5, 26)
   },
   {
     id: 15,
-    dot: {
-      color: "red",
-      class: "my-dot-class"
-    },
-    popover: {
-      label: "Coding"
-    },
+    color: 0,
+    label: "Coding",
     dates: new Date()
   },
   {
     id: 16,
-    dot: {
-      color: "green"
-    },
-    popover: {
-      label: "Workout"
-    },
+    color: 1,
+    label: "Workout",
     dates: new Date()
+  },
+  {
+    id: 17,
+    color: 2,
+    label: "Reading Books",
+    dates: new Date(2020, 5, 21)
   }
 ];
 
