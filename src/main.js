384dbc2ff2274332e83ea2d85e284cb8d9fdc568
yimsb01 <yimsb02@naver.com>
2020-06-17 22:29:34 +0900

    Add infinite loading

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import vcalendar from "./plugins/vcalendar";
+import InfiniteLoading from "vue-infinite-loading";
 
 Vue.config.productionTip = false;
+Vue.use(InfiniteLoading);
 
 new Vue({
   router,
